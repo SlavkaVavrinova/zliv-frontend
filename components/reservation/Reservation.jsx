fbf2e96e41e13e9b27967c06301915ac771a3169
Stanislava Vavřinová <slavka.vav@gmail.com>
2023-03-10 11:06:10 +0100

    form

--- components/reservation/Reservation.jsx
+++ components/reservation/Reservation.jsx
@@ -1,23 +1,63 @@
 import Head from 'next/head';
 import Link from "next/link";
 import ReservationForm from "./ReservationForm";
+import {useEffect, useState} from "react";
 
 
 const Reservation = () => {
+
+    const [terms, setTerms] = useState();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const nacistData = async () => {
+            try {
+                const resp = await fetch("http://localhost/zlivDoctrine/www/api/");
+                switch (resp.status) {
+                    case 200:
+                        const data = await resp.json();
+                        setTerms(data);
+                        break;
+                    case 500:
+                        alert("Server vrátil neočekávanou chybu.");
+                        break;
+                    case 503:
+                        alert("Server je přetížen.");
+                        break;
+                }
+            } catch (error) {
+                console.error("Chyba komunikace se serverem:", error.message);
+                alert("Chyba komunikace se serverem. Jste připojeni k internetu?");
+            } finally {
+                setLoading(false)
+            }
+        };
+        nacistData();
+    }, []);
+
+    const termsString = JSON.stringify(terms);
     return (
-        <>
-
-
             <main className="content">
                 <h1>Rezervace</h1>
                 <p>Máte zájem o ubytování v naší chatě? Pro výběr vhodného termínu nám zanechte zprávu nebo nám pošlete
                     email na adresu <Link href="mailto:chata.ruzenka@seznam.cz">chata.ruzenka@seznam.cz</Link>. Náš objekt je k dispozici k pronájmu od soboty
                     do soboty. Pro kratší pobyty než týden, v mimo sezónním období, nás neváhejte kontaktovat kvůli
                     individuálnímu jednání. Děkujeme Vám za projevený zájem.</p>
+                <ReservationForm terms={terms} loading={loading}/>
 
-                <ReservationForm/>
+                {loading ? (
+                    <div className="loading">Načítám data
+                        <div className="spinner"></div>
+                    </div>
+                ) : (
+                    <>
+                        {terms.map(term => (
+                            <div key={term.id} className={`background background${term.status}`}>{term.term}</div>
+                        ))}
+                    </>
+                )}
             </main>
-        </>
+
     );
 };
 
--- components/reservation/ReservationForm.jsx
+++ components/reservation/ReservationForm.jsx
@@ -1,127 +1,199 @@
-import {useEffect, useState} from "react";
+import { useFormik } from 'formik';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+
+const ReservationForm = ({ terms, loading }) => {
 
 
-const ReservationForm = () => {
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            email: '',
+            phone: '',
+            date: 'Vyber ',
+            info: '',
+        },
+        onSubmit: async (values) => {
+            const url = 'http://localhost/zliv/www/api/request';
+            const data = {
+                name: values.name,
+                email: values.email,
+                phone: values.phone,
+                date: values.date,
+                info: values.message,
+            };
 
-    const [terms, setTerms] = useState();
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-       const nacistData = async () => {
             try {
-                const resp = await fetch("http://localhost/zliv/www/api/");
-                switch (resp.status) {
-                    case 200:
-                        const data = await resp.json();
-                        setTerms(data);
-                        break;
-                    case 500:
-                        alert("Server vrátil neočekávanou chybu.");
-                        break;
-                    case 503:
-                        alert("Server je přetížen.");
-                        break;
+                const response = await fetch(url, {
+                    method: 'POST',
+                    body: JSON.stringify(data),
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                });
+
+                if (response.ok) {
+                    console.log('Formulář úspěšně odeslán.');
+                } else {
+                    console.error('Chyba při odesílání formuláře.');
                 }
             } catch (error) {
-                console.error("Chyba komunikace se serverem:", error.message);
-                alert("Chyba komunikace se serverem. Jste připojeni k internetu?");
-            } finally {
-                setLoading(false)
+                console.error(error);
             }
-        };
-        nacistData();
-   }, []);
+        },
+    });
 
-   const termsString = JSON.stringify(terms);
+    return (
+        <Formik
+            initialValues={{ name: '', email: '', phone: '', date: '' , info:''}}
+            validate={values => {
+                const errors = {};
+                if (!values.email) {
+                    errors.email = 'Required';
+                } else if (
+                    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                ) {
+                    errors.email = 'Invalid email address';
+                }
+                return errors;
+            }}
+            onSubmit={(values, { setSubmitting }) => {
+                setTimeout(() => {
+                    alert(JSON.stringify(values, null, 2));
+                    setSubmitting(false);
+                }, 400);
+            }}
+        >
+            {({
+                  values,
+                  errors,
+                  touched,
+                  handleChange,
+                  handleBlur,
+                  handleSubmit,
+                  isSubmitting,
+                  /* and other goodies */
+              }) => (
+                <form onSubmit={handleSubmit}>
+                    <div className="form__field">
 
-   return (
-       <form id="reservationForm" className="form">
-           {loading ? (
-               <div className="loading">Načítám data
-                   <div className="spinner"></div>
-               </div>
-           ) : (
-               <p className="class">{termsString}</p>
-           )}
+                    <label className="form__label" htmlFor="name">
+                        Jméno a příjmení <span className="required">*</span>
+                    </label>
+                    <input
+                        className="form__input"
+                        type="text"
+                        name="name"
+                        id="name"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.name}
+                        required
+                    />
+                    </div>
+
+                    <div className="form__field">
+                        <label className="form__label" htmlFor="email">
+                            E-mail <span className="required">*</span>
+                        </label>
+                        <input
+                            className="form__input"
+                            type="email"
+                            name="email"
+                            id="email"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.email}
+                            required
+                        />
+                    </div>
+
+                    {errors.email && touched.email && errors.email}
+
+                    <div className="form__field">
+                        <label className="form__label" htmlFor="phone">
+                            Telefon
+                        </label>
+                        <input
+                            className="form__input"
+                            type="tel"
+                            name="phone"
+                            id="phone"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.phone}                        />
+                    </div>
+
+                    <p>
+                        V případě zájmu o kratší termín či dva týdny, vyberte přibližný týden a
+                        vyplňte upřesnění níže. Ostatní termíny jsou bohužel obsazeny.
+                    </p>
+
+                    <div className="form__field">
+                        <label className="form__label" htmlFor="date">
+                            Termín rezervace
+                        </label>
+                        <Field name="date" as="select"
+                               className="form__select"
+
+                               id="date"
+                               onChange={handleChange}
+                               onBlur={handleBlur}
+                               value={values.date}
+                        >
+                            <option value="">Vyber
+                            </option>
+                            {loading ? (
+                                    <span className="spinner"></span>
+                            ) : (
+                                <>
+                                    {terms.map((term) => {
+                                        if (term.status === 1) {
+                                            return (
+                                                <option key={term.id} value={term.term}>
+                                                    {term.term}
+                                                </option>
+                                            );
+                                        }
+                                        return null;
+                                    })}
+                                </>
+                            )}
+                        </Field>
+
+                    </div>
+
+                    <div className="form__field">
+                        <label className="form__label" htmlFor="info">
+                            Chcete něco dalšího sdělit
+                        </label>
+                        <textarea
+                            className="form__textarea"
+                            name="info"
+                            id="info"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.info}
+                            rows="5"
+                            cols="20"
+                            maxLength="2000"
+                        ></textarea>
+                    </div>
+                    <p className="text-right small">
+                        <span className="required">*</span> Požadované údaje
+                    </p>
+
+                    <div className="form__button">
+                        <button className="btn" type="submit" name="send" id="send" disabled={isSubmitting}>
+                            Odeslat poptávku
+                        </button>
+                    </div>
 
 
-           <div className="form__field">
-               <label className="form__label" htmlFor="name">
-                   Jméno a příjmení <span className="required">*</span></label
-               >
-               <input
-                   className="form__input"
-                   type="text"
-                   name="jmeno"
-                   id="name"
-                   required
-               />
-           </div>
+                </form>
+            )}
+        </Formik>
 
-           <div className="form__field">
-               <label className="form__label" htmlFor="email">
-                   E-mail <span className="required">*</span></label
-               >
-               <input
-                   className="form__input"
-                   type="email"
-                   name="email"
-                   id="email"
-                   required
-                   value="@"
-               />
-           </div>
-
-           <div className="form__field">
-               <label className="form__label" htmlFor="tel"> Telefon </label>
-               <input
-                   className="form__input"
-                   type="tel"
-                   name="tel"
-                   id="tel"
-                   value="+420"
-               />
-           </div>
-
-           <p>V případě zájmu o kratší termín či dva týdny, vyberte přibližný týden a vyplňte upřesnění níže. Ostatní
-               termíny jsou bohužel obsazeny.</p>
-
-           <div className="form__field">
-               <label className="form__label" htmlFor="date">Termín rezervace</label>
-               <select className="form__select" name="date" id="date">
-                   <option value="1">1</option>
-                   <option value="2">2</option>
-                   <option value="3">3</option>
-
-               </select>
-           </div>
-
-           <div className="form__field">
-               <label className="form__label" htmlFor="message"
-               >Chcete něco dalšího sdělit
-               </label>
-               <textarea
-                   className="form__textarea"
-                   name="zprava"
-                   id="message"
-                   rows="5"
-                   cols="20"
-                   maxLength="2000"
-               ></textarea>
-           </div>
-           <p className="text-right small">
-               <span className="required">*</span> Požadované údaje
-           </p>
-
-           <div className="error-message" id="error-message"></div>
-
-           <div className="form__button">
-               <button className="btn" type="submit" name="send" id="send">
-                   Odeslat poptávku
-               </button>
-           </div>
-       </form>
-   );
+    );
 };
 
 export default ReservationForm;
